0b492498cfd70ab5c24370d706fdcc0098d7c4e9
Roshan Gurung <roshc10grg@gmail.com>
2024-06-25 15:10:33 +0545

    updates

--- lib/game.dart
+++ lib/game.dart
@@ -1,4 +1,5 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
+import 'dart:math';
 
 class GamePage extends StatefulWidget {
   const GamePage({super.key});
@@ -11,78 +12,230 @@ class _GamePageState extends State<GamePage> {
   String photo1 = "assets/Images/rock.jpg";
   String photo2 = "assets/Images/konan.jpg";
   String photo3 = "assets/Images/scissor.jpg";
-  
-  String result ='';
+  String compick = '';
+  String humanpick = '';
+  var result = '';
+  var random = Random();
+  String compPickPhoto = '';
+  String humanPickPhoto = '';
 
-  void click1(){
+  void click1() {
     setState(() {
-          result = "You Picked Rock";
+      var randomnum = random.nextInt(9);
+      humanpick = 'Rock';
+      humanPickPhoto = photo1;
+
+      if (randomnum >= 0 && randomnum < 3) {
+        compick = 'Rock';
+        compPickPhoto = photo1;
+      } else if (randomnum >= 3 && randomnum < 6) {
+        compick = 'Paper';
+        compPickPhoto = photo2;
+      } else if (randomnum >= 6 && randomnum <= 8) {
+        compick = 'Scissor';
+        compPickPhoto = photo3;
+      }
+
+      if (compick == 'Rock') {
+        result = 'Draw';
+      } else if (compick == 'Paper') {
+        result = 'You Lose';
+      } else if (compick == 'Scissor') {
+        result = 'You Won';
+      }
     });
   }
-   void click2(){
+
+  void click2() {
     setState(() {
-      result ='You Picked Papper';
-      print(result);
+      var randomnum = random.nextInt(9);
+      humanpick = 'Paper';
+      humanPickPhoto = photo2;
+
+      if (randomnum >= 0 && randomnum < 3) {
+        compick = 'Rock';
+        compPickPhoto = photo1;
+      } else if (randomnum >= 3 && randomnum < 6) {
+        compick = 'Paper';
+        compPickPhoto = photo2;
+      } else if (randomnum >= 6 && randomnum <= 8) {
+        compick = 'Scissor';
+        compPickPhoto = photo3;
+      }
+
+      if (compick == 'Rock') {
+        result = 'You Won';
+      } else if (compick == 'Paper') {
+        result = 'Draw';
+      } else if (compick == 'Scissor') {
+        result = 'You Lose';
+      }
     });
   }
-   void click3(){
+
+  void click3() {
     setState(() {
-      result ='You Picked Scissor';
-      print(result);
+      var randomnum = random.nextInt(9);
+      humanpick = 'Scissor';
+      humanPickPhoto = photo3;
+
+      if (randomnum >= 0 && randomnum < 3) {
+        compick = 'Rock';
+        compPickPhoto = photo1;
+      } else if (randomnum >= 3 && randomnum < 6) {
+        compick = 'Paper';
+        compPickPhoto = photo2;
+      } else if (randomnum >= 6 && randomnum <= 8) {
+        compick = 'Scissor';
+        compPickPhoto = photo3;
+      }
+
+      if (compick == 'Rock') {
+        result = 'You Lose';
+      } else if (compick == 'Paper') {
+        result = 'You Won';
+      } else if (compick == 'Scissor') {
+        result = 'Draw';
+      }
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    
-    return  Scaffold(
+    return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Text("RSP-Game",style: TextStyle(color: Colors.white),),
+        title: Text(
+          "RSP-Game",
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
         actions: [
-          IconButton(onPressed: null, icon: Icon(Icons.menu,color: Colors.white,)),
+          IconButton(
+            onPressed: null,
+            icon: Icon(Icons.menu, color: Colors.white),
+          ),
         ],
       ),
       body: Column(
         children: [
-          SizedBox(height: 20,),
-          Row(mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Rock Papper Scissor ?",style: TextStyle(fontSize: 15),),
-            ],
-          ),
-          SizedBox(height: 40,),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-            GestureDetector(
-              onTap:  click1,
-              child: Container(
-                 width: 100,
-                 height: 100,
-                child: Image.asset(photo1),),
-            ),  
-               GestureDetector(onTap: click2,
-                 child: Container(
-                  width: 100,
-                  height: 100,
-                  child: Image.asset(photo2)),
-               ),
-               GestureDetector(
-                onTap: click3,
-                 child: Container(
-                  width: 100,
-                  height: 100,
-                  child: Image.asset(photo3)),
-               )
-          ],
-          
-          ),
-          SizedBox(height: 20,),
+          SizedBox(height: 20),
           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(result,style: TextStyle(fontSize: 20),),
+              Text(
+                "Click an Image to choose a Weapon.",
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
             ],
-          )
+          ),
+          SizedBox(height: 40),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              GestureDetector(
+                onTap: click1,
+                child: Column(
+                  children: [
+                    Container(
+                      width: 120,
+                      height: 120,
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage(photo1),
+                        radius: 80,
+                      ),
+                    ),
+                    SizedBox(height: 15,),
+                    Text("Rock",style: TextStyle(fontSize: 15,color: Colors.white),)
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: click2,
+                child: Column(
+                  children: [
+                    Container(
+                      width: 120,
+                      height: 120,
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage(photo2),
+                        radius: 100,
+                      ),
+                    ),
+                    SizedBox(height: 15,),
+                    Text("Paper",style: TextStyle(fontSize: 15,color: Colors.white),)
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: click3,
+                child: Column(
+                  children: [
+                    Container(
+                      width: 120,
+                      height: 120,
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage(photo3),
+                        radius: 80,
+                      ),
+                    ),
+                    SizedBox(height: 15,),
+                    Text("Scissor",style: TextStyle(fontSize: 15,color: Colors.white),)
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 80),
+          // Updated part below
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
+            
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      "You Pick: $humanpick",
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,color: Colors.white),
+                    ),
+                    SizedBox(height: 20),
+                    humanPickPhoto.isNotEmpty
+                        ? CircleAvatar(
+                            backgroundImage: AssetImage(humanPickPhoto),
+                            radius: 60,
+                          )
+                        : Container(),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "Computer Pick: $compick",
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,color: Colors.white),
+                    ),
+                    SizedBox(height: 20),
+                    compPickPhoto.isNotEmpty
+                        ? CircleAvatar(
+                            backgroundImage: AssetImage(compPickPhoto),
+                            radius: 60,
+                          )
+                        : Container(),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 50),
+          Container(
+            child: Text(
+              result,
+              style: TextStyle(
+                  fontSize: 24, color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+          ),
         ],
       ),
     );
